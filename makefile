ded30a7d1d7341134e566a6e00681cf17d0aca43
Alexandre de Magalhaes <alexandredemagalhaess@gmail.com>
2024-12-27 12:03:03 -0300

    update makefile for publish

--- makefile
+++ makefile
@@ -1,6 +1,10 @@
 build:
 	python setup.py sdist bdist_wheel
 
+publish:
+	python3 -m twine upload --skip-existing dist/*
+
+.PHONY: all test clean
 test:
 	pip3 install -r requirements_test.txt
 	pytest -v
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='psdq',
     packages=find_packages(),
-    version='0.1.3',
+    version='0.1.4',
     description='Ad Hoc Data Quality Tool for PySpark',
     author='Alexandre "Alex" de Magalhaes',
     author_email="alexandredemagalhaess@gmail.com",
